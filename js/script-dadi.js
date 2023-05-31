e05172d64aa98ef99825d9b8f91c66ee71290e3a
Michel-Ramirez <michel.ramirez19@gmail.com>
2023-05-31 18:17:01 +0200

    add dadi game

---
+++ js/script-dadi.js
@@ -0,0 +1,21 @@
+console.log('JS ok')
+
+const button = document.getElementById('btn-dadi');
+const resultUser = documenti.getElementById('dado-utente');
+const resultCpu = document.getElementById('dado-cpu');
+
+
+button.addEventListener('click', function(){
+
+    const userDado = ''
+
+    for (let i = 0; i < 6; i++){
+        userDado = Math.floor((Math.random() * 6) + 1)
+    }
+
+
+
+
+});    
+
+
