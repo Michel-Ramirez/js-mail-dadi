d202b57481844755a8bb861fdf34da15bdfd1aad
Michel-Ramirez <michel.ramirez19@gmail.com>
2023-05-31 15:25:21 +0200

    fix id in js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ console.log('JS ok')
 
 // recupero gli elementi dal dom
 const emailInput = document.getElementById('user-email');
-const button = document.getElementById('btn-submit');
+const button = document.getElementById('button');
 
 //creo la mia lista aray
 const emailsList = ['giovanni.marci@gmail.com', 'pietro.sampini@gmail.com', 'alfredo.sappi@hotmail.it', 'pippo.ciccio@mail.it', 'michel.ramirez@gmail.com', 'michelramirez@outlook.it']
