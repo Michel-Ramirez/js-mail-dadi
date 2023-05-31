2dbbda8aaf72ed7503eead6c990874145ac4ea1d
Michel-Ramirez <michel.ramirez19@gmail.com>
2023-05-31 15:21:34 +0200

    creazione

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 console.log('JS ok')
+
+// recupero gli elementi dal dom
+const emailInput = document.getElementById('user-email');
+const button = document.getElementById('btn-submit');
+
+//creo la mia lista aray
+const emailsList = ['giovanni.marci@gmail.com', 'pietro.sampini@gmail.com', 'alfredo.sappi@hotmail.it', 'pippo.ciccio@mail.it', 'michel.ramirez@gmail.com', 'michelramirez@outlook.it']
+
+//Al click del bottono rivero la stringe immessa dal utente
+
+button.addEventListener('click', function(){
+    
+});
